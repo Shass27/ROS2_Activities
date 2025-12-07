ecddde083785f7c6119e4b5c5abca45416984503
Shass27 <shaswaths@icloud.com>
2025-12-08 00:51:56 +0530

    Added server to the number_counter node

--- Services_Act/src/number_counter/src/number_counter.cpp
+++ Services_Act/src/number_counter/src/number_counter.cpp
@@ -1,21 +1,44 @@
 #include "rclcpp/rclcpp.hpp"
 #include "example_interfaces/msg/int64.hpp"
+#include "example_interfaces/srv/set_bool.hpp"
+
+using namespace std::placeholders;
 
 class number_counternode : public rclcpp::Node {
     rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr publisher_;
     rclcpp::Subscription<example_interfaces::msg::Int64>::SharedPtr subscriber_;
+    rclcpp::Service<example_interfaces::srv::SetBool>::SharedPtr server_;
+    int number = 0;
     public:
         number_counternode() : Node("number_counter") {
             subscriber_ = create_subscription<example_interfaces::msg::Int64> (
                 "number", 10, std::bind(&number_counternode::getnum, this, std::placeholders::_1));
             publisher_ = create_publisher<example_interfaces::msg::Int64>("number_count", 10);
+            server_ = create_service<example_interfaces::srv::SetBool>("reset_counter",
+                    std::bind(&number_counternode::reset, this, _1, _2));
             RCLCPP_INFO(get_logger(), "Number Counter started");
         }
     private:
         void getnum (const example_interfaces::msg::Int64::SharedPtr msg) {
+            number += msg->data;
             auto newMsg = example_interfaces::msg::Int64();
-            newMsg.data = msg->data;
+            newMsg.data = number;
             publisher_->publish(newMsg);
+        }
+        void reset(
+                    const example_interfaces::srv::SetBool::Request::SharedPtr request,
+                    example_interfaces::srv::SetBool::Response::SharedPtr response)
+        {
+            if (request->data){
+                number=0;
+                response->success=true;
+                response->message = "Reset succesful";
+            }
+            else {
+                response->success=false;
+                response->message = "Reset failed";
+            }
+
         }
 };
 
