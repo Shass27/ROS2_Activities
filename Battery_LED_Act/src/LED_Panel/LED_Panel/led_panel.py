f1d547071436efd3b780006ffe8d1dd8babd2827
Shass27 <shaswaths@icloud.com>
2025-12-12 20:30:30 +0530

    Added rqt graph

--- Battery_LED_Act/src/LED_Panel/LED_Panel/led_panel.py
+++ Battery_LED_Act/src/LED_Panel/LED_Panel/led_panel.py
@@ -9,14 +9,8 @@ class LEDPanelNode(Node):
         super().__init__("led_panel")
         self.led_stat = [0,0,0]
         self.led_state_publisher = self.create_publisher(LEDStat, "led_panel_state", 10)
-        self.led_states_timer = self.create_timer(5, self.publish_led_stats)
         self.set_led_service = self.create_service(ModLED, "set_led", self.callback_set_led)
         self.get_logger().info("LED Panel node has started.")
-
-    def publish_led_stats(self):
-        msg = LEDStat()
-        msg.ledstat = self.led_stat
-        self.led_state_publisher.publish(msg)
 
     def callback_set_led(self, request: ModLED.Request, response: ModLED.Response):
         if (request.state==True):
